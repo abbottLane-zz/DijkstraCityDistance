b9467c3c284d070b593f8f68b87b78ed32a0088f
William <will@asklane.com>
2014-08-11 15:52:24 -0600

    Initial commit

---
+++ src/main/java/Edge.java
@@ -0,0 +1,10 @@
+/**
+ * Created by wlane on 8/11/14.
+ */
+class Edge
+{
+    public final Vertex target;
+    public final double weight;
+    public Edge(Vertex argTarget, double argWeight)
+    { target = argTarget; weight = argWeight; }
+}
